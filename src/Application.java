9048a990ae5600ff70d776c009d0dcb0893cf1de
CibeleR <cibele.rodrigues17@hotmail.com>
2025-03-04 08:00:11 -0300

    Application iniciado

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
 
 public class Application {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int menu = 0;
+		
+		while (menu != 5) {
+			
+			System.out.println();
+			System.out.println("-----------------------------------------");
+			System.out.println("TAREFAS");
+			
+			System.out.println();
+			System.out.println("Menu:");
+			System.out.println("1 - Criar");
+			System.out.println("2 - Deletar");
+			System.out.println("3 - Buscar lista de tarefas por funcionário");
+			System.out.println("4 - Atualizar status da tarefa");
+			System.out.println("5 - Fechar programa");
+			System.out.println();
+			System.out.println("O que você deseja fazer?");
+			System.out.print("Digite o ID da ação: ");
+			menu = sc.nextInt();
+			
+			
+		}
+		sc.close();
+		
+	}
 }
